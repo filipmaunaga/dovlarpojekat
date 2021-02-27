2ca4d02637c5ad3c07ce6ad13a46a33d18797148
Filip Maunaga <57533924+filipmaunaga@users.noreply.github.com>
2021-02-27 18:51:42 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app=express();
+app.get("/", function (req,res){
+  res.send("<h1>Hello world! </h1>");
+});
+app.listen(3000,function (){
+  console.log("Server is now running on port 3000");
+})
