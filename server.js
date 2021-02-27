eada7f98ff4e2552cbfea0636c11ef16b5f7388f
Filip Maunaga <57533924+filipmaunaga@users.noreply.github.com>
2021-02-27 18:52:51 +0100

    Update server.js
    I added console.log

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/", function (req,res){
   res.send("<h1>Hello world! </h1>");
+  console.log(req);
 });
 app.listen(3000,function (){
   console.log("Server is now running on port 3000");
